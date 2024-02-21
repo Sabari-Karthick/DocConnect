5e9b71d0106d3f5d407bce32161cc8004b09dc6f
Karthick9902 <126456084+Sabari-Karthick@users.noreply.github.com>
2024-02-21 19:31:19 +0530

    Update UserController.java

--- src/main/java/com/Batman/restcontroller/UserController.java
+++ src/main/java/com/Batman/restcontroller/UserController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,6 +33,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
+@PreAuthorize("hasAuthority('ADMIN','USER')")
 public class UserController {
 	
 	private final IUserService userService;
