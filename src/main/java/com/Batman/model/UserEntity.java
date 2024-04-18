acd86139d67e878df682ecff217b8fdd14c1c02b
Karthick9902 <126456084+Sabari-Karthick@users.noreply.github.com>
2024-04-18 15:51:54 +0530

    Update UserEntity.java

--- src/main/java/com/Batman/model/UserEntity.java
+++ src/main/java/com/Batman/model/UserEntity.java
@@ -2,22 +2,21 @@ package com.Batman.model;
 
 import java.util.Set;
 
-import javax.persistence.CollectionTable;
-import javax.persistence.Column;
-import javax.persistence.ElementCollection;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.Table;
-
 import com.Batman.enums.AuthenticationProiver;
 import com.Batman.enums.Role;
 
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
