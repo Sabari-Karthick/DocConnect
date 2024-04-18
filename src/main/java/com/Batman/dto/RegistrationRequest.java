e64c486ec1c690dfdbf72c029bb3915baaf8f1a7
Karthick9902 <126456084+Sabari-Karthick@users.noreply.github.com>
2024-04-18 17:11:34 +0530

    Update RegistrationRequest.java

--- src/main/java/com/Batman/dto/RegistrationRequest.java
+++ src/main/java/com/Batman/dto/RegistrationRequest.java
@@ -1,8 +1,8 @@
 package com.Batman.dto;
 
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 import lombok.Data;
 
