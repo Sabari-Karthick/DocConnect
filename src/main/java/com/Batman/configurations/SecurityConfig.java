bdb875de485faa0de2adcffcb1d4c46e9c0c1749
Karthick9902 <126456084+Sabari-Karthick@users.noreply.github.com>
2024-04-18 15:49:08 +0530

    Update SecurityConfig.java
    Changed the deprecated methods to new spring security 6 syntax

--- src/main/java/com/Batman/configurations/SecurityConfig.java
+++ src/main/java/com/Batman/configurations/SecurityConfig.java
@@ -7,8 +7,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import com.Batman.security.jwt.JwtConfigurer;
+import com.Batman.security.jwt.JwtFilter;
 import com.Batman.security.oauth.CustomOauth2Service;
 import com.Batman.security.oauth.OAuth2SuccessHandler;
 
@@ -22,34 +23,23 @@ public class SecurityConfig {
 
 	private final CustomOauth2Service oauth2Service;
 
-	private final JwtConfigurer configurer;
+	private final JwtFilter jwtFilter;
 
 	private final OAuth2SuccessHandler handler;
-	
-
 
 	@Bean
-	 SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http
-                .csrf(csrf -> csrf.disable())
-                .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(requests -> requests
-                        .antMatchers("/oauth2/**", "/**/*swagger*/**").permitAll()
-                        .antMatchers("/api/user/login", "/api/user/register").permitAll()
-                        .antMatchers("/api/doctor/all/**").permitAll()
-                        .anyRequest().authenticated())
-                .oauth2Login(login -> login
-                        .authorizationEndpoint().baseUri("/oauth2/authorize")
-                        .and()
-                        .userInfoEndpoint().userService(oauth2Service)
-                        .and()
-                        .successHandler(handler))
-                .apply(configurer);
-             
-		     
-		  
-	
+		http.authorizeHttpRequests((request) -> request.requestMatchers("/oauth2/**", "/**/*swagger*/**").permitAll()
+				.requestMatchers("/api/user/login", "/api/user/register").permitAll()
+				.requestMatchers("/api/doctor/all/**").permitAll().anyRequest().authenticated())
+				.sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.csrf((csrf) -> csrf.disable())
+				.oauth2Login((login) -> login
+						.authorizationEndpoint((authorization) -> authorization.baseUri("/oauth2/authorize"))
+						.userInfoEndpoint((userInfo) -> userInfo.userService(oauth2Service)).successHandler(handler))
+				.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
 		return http.build();
 	}
 }
