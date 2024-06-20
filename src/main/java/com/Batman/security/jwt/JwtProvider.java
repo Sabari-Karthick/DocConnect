c1002894b92e8d6107884d83ac1319d8224f1b91
Karthick9902 <126456084+Sabari-Karthick@users.noreply.github.com>
2024-06-20 16:56:49 +0530

    Update JwtProvider.java

--- src/main/java/com/Batman/security/jwt/JwtProvider.java
+++ src/main/java/com/Batman/security/jwt/JwtProvider.java
@@ -23,61 +23,65 @@ import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class JwtProvider {
 
-    @Lazy
-    private final UserDetailsService userDetailsService;
+	@Lazy
+	private final UserDetailsService userDetailsService;
 
-    @Value("${jwt.header}")
-    private String authorizationHeader;
+	@Value("${jwt.header}")
+	private String authorizationHeader;
 
-    @Value("${jwt.secret}")
-    private String secretKey;
+	@Value("${jwt.secret}")
+	private String secretKey;
 
-    @Value("${jwt.expiration}")
-    private long validityInMilliseconds;
+	@Value("${jwt.expiration}")
+	private long validityInMilliseconds;
 
-    @PostConstruct
-    protected void init() {
-        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
-    }
+	@PostConstruct
+	protected void init() {
+		secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+	}
 
-    public String createToken(String usermail, String role) {
-        Claims claims = Jwts.claims().setSubject(usermail);
-        claims.put("role", role);
-        Date now = new Date();
-        Date validity = new Date(now.getTime() + validityInMilliseconds * 1000);
+	public String createToken(String usermail, String role) {
+		Claims claims = Jwts.claims().setSubject(usermail);
+		claims.put("role", role);
+		Date now = new Date();
+		Date validity = new Date(now.getTime() + validityInMilliseconds * 1000);
 
-        return Jwts.builder()
-                .setClaims(claims)
-                .setIssuedAt(now)
-                .setExpiration(validity)
-                .signWith(SignatureAlgorithm.HS256, secretKey)
-                .compact();
-    }
+		return Jwts.builder().setClaims(claims).setIssuedAt(now).setExpiration(validity)
+				.signWith(SignatureAlgorithm.HS256, secretKey).compact();
+	}
 
-    public boolean validateToken(String token) {
-        try {
-            Jws<Claims> claimsJws = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
-            return !claimsJws.getBody().getExpiration().before(new Date());
-        } catch (JwtException | IllegalArgumentException exception) {
-            throw new JwtAuthenticationException("INVALID_TOKEN", HttpStatus.UNAUTHORIZED);
-        }
-    }
+	public boolean validateToken(String token) {
+		try {
+			log.info("Validating Token......");
+			Jws<Claims> claimsJws = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+			return !claimsJws.getBody().getExpiration().before(new Date());
+		} catch (JwtException | IllegalArgumentException exception) {
+			log.error("Token Validation Failed......" + exception.getMessage());
+			throw new JwtAuthenticationException("INVALID_TOKEN", HttpStatus.UNAUTHORIZED);
+		}
+	}
 
-    public Authentication getAuthentication(String token) {
-        UserDetails userDetails = this.userDetailsService.loadUserByUsername(getUsermail(token));
-        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
-    }
+	public Authentication getAuthentication(String token) {
+		UserDetails userDetails = this.userDetailsService.loadUserByUsername(getUsermail(token));
+		return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+	}
 
-    public String getUsermail(String token) {
-        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
-    }
+	public String getUsermail(String token) {
+		return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+	}
 
-    public String resolveToken(HttpServletRequest request) {
-        return request.getHeader(authorizationHeader);
-    }
+	public String resolveToken(HttpServletRequest request) {
+		String bearerToken = request.getHeader(authorizationHeader);
+		if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+			return bearerToken.substring(7);
+		}
+		return null;
+	}
 }
