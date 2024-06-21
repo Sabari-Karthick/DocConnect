7a2156c589866da2bebaae4c1d2c75409dcd1f24
Karthick9902 <126456084+Sabari-Karthick@users.noreply.github.com>
2024-06-21 10:00:11 +0530

    Update LogManagementAspect.java

--- src/main/java/com/Batman/aspect/LogManagementAspect.java
+++ src/main/java/com/Batman/aspect/LogManagementAspect.java
@@ -15,7 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class LogManagementAspect {
 
-	@Pointcut(value = "execution(* com.Batman.restcontroller.*.*(..))")
+	@Pointcut(value = "execution(* com.Batman.restcontroller.*.*(..)) || execution(* com.Batman.advice.*.*(..))")
 	public void executeLogs() {
 
 	}
