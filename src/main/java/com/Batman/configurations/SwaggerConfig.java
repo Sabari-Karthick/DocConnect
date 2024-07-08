a234afc3db1a843a6c39681317dfe389964db47f
SK <sabariks2003@gmail.com>
2024-07-08 22:53:06 +0530

    Version Update

--- src/main/java/com/Batman/configurations/SwaggerConfig.java
+++ src/main/java/com/Batman/configurations/SwaggerConfig.java
@@ -1,37 +1,37 @@
-package com.Batman.configurations;
-
-import java.util.Collections;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-@EnableSwagger2
-@Configuration
-public class SwaggerConfig {
-	@Bean
-	 Docket createDocket() {
-		
-		return new Docket(DocumentationType.SWAGGER_2).
-	                  select().
-	                  apis(RequestHandlerSelectors.basePackage("com.Batman.restcontroller")).
-	                  paths(PathSelectors.any()).
-	                  build().
-	                  useDefaultResponseMessages(true).apiInfo(getAPIInfo());
-	
-	}
-	
-	
-	private ApiInfo getAPIInfo() {
-		return new ApiInfo("Doctor", "DocConnect- A platform to connect patients and doctors", "1.0 Release", "http:", new Contact("Karthick", "http://Zack/DocConnect", "sabriks2063@gmail.com"), "Wayne Enterprise", "license.com", Collections.emptyList());
-	}
-	
-
-}
+//package com.Batman.configurations;
+//
+//import java.util.Collections;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import springfox.documentation.builders.PathSelectors;
+//import springfox.documentation.builders.RequestHandlerSelectors;
+//import springfox.documentation.service.ApiInfo;
+//import springfox.documentation.service.Contact;
+//import springfox.documentation.spi.DocumentationType;
+//import springfox.documentation.spring.web.plugins.Docket;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//
+//@EnableSwagger2
+//@Configuration
+//public class SwaggerConfig {
+//	@Bean
+//	 Docket createDocket() {
+//		
+//		return new Docket(DocumentationType.SWAGGER_2).
+//	                  select().
+//	                  apis(RequestHandlerSelectors.basePackage("com.Batman.restcontroller")).
+//	                  paths(PathSelectors.any()).
+//	                  build().
+//	                  useDefaultResponseMessages(true).apiInfo(getAPIInfo());
+//	
+//	}
+//	
+//	
+//	private ApiInfo getAPIInfo() {
+//		return new ApiInfo("Doctor", "DocConnect- A platform to connect patients and doctors", "1.0 Release", "http:", new Contact("Karthick", "http://Zack/DocConnect", "sabriks2063@gmail.com"), "Wayne Enterprise", "license.com", Collections.emptyList());
+//	}
+//	
+//
+//}
